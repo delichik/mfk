9c5dccab6167667588524af2ffc34762cedea5a4
delichik <iiii_miku@hotmail.com>
2023-03-24 16:21:06 +0800

    remove objective; support auto load

--- app/app.go
+++ app/app.go
@@ -14,31 +14,26 @@ import (
 	"github.com/delichik/mfk/logger"
 )
 
-type App struct {
-	ctx    context.Context
-	cancel context.CancelFunc
+var (
+	ctx                 context.Context
+	cancel              context.CancelFunc
+	cm                  *config.Manager
+	afterRunCall        func()
+	modules             map[string]Module
+	orderedModules      []Module
+	autoLoadModuleCount int
+)
 
-	cm             *config.Manager
-	afterRunCall   func()
-	modules        map[string]Module
-	orderedModules []Module
+func init() {
+	ctx, cancel = context.WithCancel(context.Background())
+	modules = map[string]Module{}
+	clvs := parseFlags()
+	cm = config.NewManager(ctx, clvs.ConfigPath)
 }
 
 type CommandLineVars struct {
 	ConfigPath string
 	Help       bool
-}
-
-func NewApp() *App {
-	ctx, cancel := context.WithCancel(context.Background())
-	app := &App{
-		ctx:     ctx,
-		cancel:  cancel,
-		modules: map[string]Module{},
-	}
-	clvs := parseFlags()
-	app.cm = config.NewManager(ctx, clvs.ConfigPath)
-	return app
 }
 
 func parseFlags() CommandLineVars {
@@ -53,17 +48,23 @@ func parseFlags() CommandLineVars {
 	return clvs
 }
 
-func (a *App) RegisterModule(module Module) {
-	a.modules[module.Name()] = module
-	a.orderedModules = append(a.orderedModules, module)
+func RegisterAutoLoadModule(module Module) {
+	modules[module.Name()] = module
+	orderedModules = append(orderedModules, module)
+	autoLoadModuleCount++
 }
 
-func (a *App) AfterRun(call func()) {
-	a.afterRunCall = call
+func RegisterModule(module Module) {
+	modules[module.Name()] = module
+	orderedModules = append(orderedModules, module)
 }
 
-func (a *App) Run() {
-	for _, module := range a.orderedModules {
+func AfterRun(call func()) {
+	afterRunCall = call
+}
+
+func Run() {
+	for _, module := range orderedModules {
 		if module.AdditionalLogger() {
 			c := logger.GetDefaultConfig()
 			c.LogPath = "logs/" + module.Name() + ".log"
@@ -71,35 +72,39 @@ func (a *App) Run() {
 		}
 	}
 
-	err := a.cm.Init()
+	err := cm.Init()
 	if err != nil {
 		log.Printf("Init config failed: %s, exit", err.Error())
 		return
 	}
-	a.cm.SetReloadCallback(a.ReloadConfig)
-	logger.InitDefault(a.cm)
-	for _, module := range a.orderedModules {
+	cm.SetReloadCallback(ReloadConfig)
+	logger.InitDefault(cm)
+	for _, module := range orderedModules {
 		if module.AdditionalLogger() {
-			logger.Init(module.Name()+"-logger", a.cm)
+			logger.Init(module.Name()+"-logger", cm)
 		}
 	}
 	logger.Info("App init")
 
 	logger.Info("Loading app modules")
-	for _, module := range a.orderedModules {
-		cfg := a.cm.GetModuleConfig(module.Name())
+	for i, module := range orderedModules {
+		cfg := cm.GetModuleConfig(module.Name())
 		if cfg == nil {
 			logger.Debug("Skip module", zap.String("name", module.Name()))
 			continue
 		}
-		logger.Debug("Applying module config", zap.String("name", module.Name()))
+		if i < autoLoadModuleCount {
+			logger.Debug("Applying auto load module config", zap.String("name", module.Name()))
+		} else {
+			logger.Debug("Applying module config", zap.String("name", module.Name()))
+		}
 		err := module.ApplyConfig(cfg)
 		if err != nil {
 			logger.Fatal("Apply module config failed, exit",
 				zap.String("name", module.Name()),
 				zap.Error(err))
 		}
-		err = module.Run(a.ctx)
+		err = module.Run(ctx)
 		if err != nil {
 			logger.Fatal("Run module failed",
 				zap.String("name", module.Name()),
@@ -107,8 +112,8 @@ func (a *App) Run() {
 		}
 	}
 	logger.Info("App modules loaded")
-	if a.afterRunCall != nil {
-		a.afterRunCall()
+	if afterRunCall != nil {
+		afterRunCall()
 	}
 
 	signalChan := make(chan os.Signal, 1)
@@ -117,18 +122,18 @@ func (a *App) Run() {
 	signal.Stop(signalChan)
 	logger.Info("App shutdown")
 
-	for _, module := range a.orderedModules {
-		cfg := a.cm.GetModuleConfig(module.Name())
+	for _, module := range orderedModules {
+		cfg := cm.GetModuleConfig(module.Name())
 		if cfg == nil {
 			continue
 		}
 		module.Exit()
 	}
-	a.cancel()
+	cancel()
 }
 
-func (a *App) ReloadConfig(name string, cfg config.ModuleConfig) {
-	module, ok := a.modules[name]
+func ReloadConfig(name string, cfg config.ModuleConfig) {
+	module, ok := modules[name]
 	if !ok {
 		return
 	}
