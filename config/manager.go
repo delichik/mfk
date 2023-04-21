d29b8d8a86561c17a954c7f3656ee2a359c9f712
delichik <iiii_miku@hotmail.com>
2023-04-21 14:20:45 +0800

    fix infinite reload

--- config/manager.go
+++ config/manager.go
@@ -16,6 +16,7 @@ type Manager struct {
 	wg   sync.WaitGroup
 
 	locker         sync.RWMutex
+	loadConfigChan chan struct{}
 	c              *Config
 	lastLoadC      *Config
 	reloadCallback ReloadCallback
@@ -23,8 +24,9 @@ type Manager struct {
 
 func NewManager(ctx context.Context, path string) *Manager {
 	return &Manager{
-		path: path,
-		ctx:  ctx,
+		path:           path,
+		ctx:            ctx,
+		loadConfigChan: make(chan struct{}, 1),
 	}
 }
 
@@ -35,12 +37,12 @@ func (m *Manager) Init() error {
 	}
 
 	m.locker.Lock()
+	defer m.locker.Unlock()
 	m.lastLoadC = newConfig
 	m.c = &Config{moduleConfigs: map[string]ModuleConfig{}}
 	for moduleName, moduleConfig := range newConfig.moduleConfigs {
 		m.c.moduleConfigs[moduleName] = moduleConfig.Clone()
 	}
-	m.locker.Unlock()
 
 	err = save(m.path, m.c)
 	if err != nil {
@@ -62,58 +64,69 @@ func (m *Manager) watchConfig() {
 	for {
 		select {
 		case <-timer.C:
-			m.locker.Lock()
-			newConfig, err := load(m.path)
-			if err != nil {
-				log.Println("Config file load failed:", err.Error(), ", abort to load")
-				m.locker.Unlock()
-				continue
-			}
-			err = check(newConfig)
-			if err != nil {
-				log.Println("Config file load failed:", err.Error(), ", abort to load")
-				m.locker.Unlock()
-				continue
-			}
-			changed := false
-			for name, lmc := range m.lastLoadC.moduleConfigs {
-				nmc := newConfig.GetModuleConfig(name)
-				if !nmc.Compare(lmc) {
-					changed = true
-					if m.reloadCallback != nil {
-						m.reloadCallback(name, nmc)
-					}
-				}
-			}
-			if changed {
-				m.c = &Config{moduleConfigs: map[string]ModuleConfig{}}
-				for moduleName, moduleConfig := range newConfig.moduleConfigs {
-					m.c.moduleConfigs[moduleName] = moduleConfig.Clone()
-				}
-			}
-
-			m.locker.Unlock()
+			m.compareAndUpdateConfig()
+		case <-m.loadConfigChan:
+			m.compareAndUpdateConfig()
 		case <-m.ctx.Done():
 			return
 		}
 	}
 }
 
+func (m *Manager) compareAndUpdateConfig() {
+	m.locker.Lock()
+	defer m.locker.Unlock()
+	newConfig, err := load(m.path)
+	if err != nil {
+		log.Println("Config file load failed:", err.Error(), ", abort to load")
+		return
+	}
+	err = check(newConfig)
+	if err != nil {
+		log.Println("Config file load failed:", err.Error(), ", abort to load")
+		return
+	}
+	changed := false
+	for name, lmc := range m.lastLoadC.moduleConfigs {
+		nmc := newConfig.GetModuleConfig(name)
+		if !nmc.Compare(lmc) {
+			changed = true
+			if m.reloadCallback != nil {
+				m.reloadCallback(name, nmc)
+			}
+		}
+	}
+	if changed {
+		m.c = &Config{moduleConfigs: map[string]ModuleConfig{}}
+		m.lastLoadC = &Config{moduleConfigs: map[string]ModuleConfig{}}
+		for moduleName, moduleConfig := range newConfig.moduleConfigs {
+			m.c.moduleConfigs[moduleName] = moduleConfig.Clone()
+			m.lastLoadC.moduleConfigs[moduleName] = moduleConfig.Clone()
+		}
+	}
+}
+
 func (m *Manager) GetModuleConfig(moduleName string) ModuleConfig {
+	m.locker.RLock()
+	defer m.locker.RUnlock()
 	return m.lastLoadC.moduleConfigs[moduleName]
 }
 
 func (m *Manager) ModifyModuleConfig(moduleName string, call func(ModuleConfig)) error {
+	m.locker.Lock()
+	defer m.locker.Unlock()
 	mc, ok := m.c.moduleConfigs[moduleName]
 	if !ok {
 		return errors.New("module config not found")
 	}
-	m.locker.Lock()
-	defer m.locker.Unlock()
 	call(mc)
 	err := save(m.path, m.c)
 	if err != nil {
 		return err
+	}
+	select {
+	case m.loadConfigChan <- struct{}{}:
+	default:
 	}
 	return nil
 }
